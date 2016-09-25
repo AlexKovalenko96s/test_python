e5f0bbeab0c931db152d387f3ab666144b7eefe7
Alex Kovalenko <alex.kovalenko.s96@gmail.com>
2016-09-25 19:14:50 +0300

    study

---
+++ fun.py
@@ -0,0 +1,6 @@
+def f(x):
+    x = 2 * x
+    return x
+
+print(f(3))
+print(f(3.3))
---
+++ test_if.py
@@ -0,0 +1,10 @@
+x = int(input("Enter some numbers: \n"))
+
+if x <= 5:
+    print("x <= 5")
+elif 5 < x <= 10:
+    print("x <= 10")
+elif 10 < x < 100:
+    print("11 < x < 100")
+else:
+    print("x > 100")
