999b9c1435de9fa02c925eb5df680e18afaf1bf3
Alex Kovalenko <alex.kovalenko.s96@gmail.com>
2016-09-25 19:42:03 +0300

    inserted_if

---
+++ inserted_if.py
@@ -0,0 +1,28 @@
+x = int(input("Enter 0..10 \n"))
+
+if x < 0:
+    print("x < 0")
+elif 0 < x <= 10:
+    if x == 1:
+        print("x = 1")
+    elif x == 2:
+        print("x = 2")
+    elif x == 3:
+        print("x = 3")
+    elif x == 4:
+        print("x = 4")
+    elif x == 5:
+        print("x = 5")
+    elif x == 6:
+        print("x = 6")
+    elif x == 7:
+        print("x = 7")
+    elif x == 8:
+        print("x = 8")
+    elif x == 9:
+        print("x = 9")
+    elif x == 10:
+        print("x = 10")
+else:
+    print("x > 10")
+    
